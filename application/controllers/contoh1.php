053555f4f5c6aff4d79f9a573d3708feaf15db39
AsepMulyana712 <asepmulyana894@yahoo.com>
2021-10-29 00:47:52 +0700

    Ngedit, sebelumnya gk bisa di push

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,17 @@
+<?php
+
+class Contoh1 extends CI_Controller
+{
+	
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama Saya : Asep Mulyana";
+		echo "NIM  : 12200659";
+		echo "Kelas : 12.3A.15";
+	}
+}
+
+
+
+  ?>
